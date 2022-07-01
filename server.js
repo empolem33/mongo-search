81a864141567ee845cf0e73bd389a6550414701a
empolem33 <inspiron0925@yahoo.com>
2022-06-30 19:38:22 -0700

    set up complete

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const {MongoClient, ObjectID} = require("mongodb")
+const { response } = require('express')
+require('dotenv').config()
+const PORT = 8000
+
+let db,
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = 'sample_mflix',
+    collection
+
+MongoClient.connect(dbConnectionStr)
+    .then(client => {
+        console.log ('Connected to database boss mode on!')
+        db = client.db(dbName)
+        collection = db.collection('movies')
+    })
+
+app.use(express.urlencoded({extended : true}))
+app.use(express.json())
+app.use(cors())
+
+app.get('/search', async (request, response) => {
+    try{
+        let result = await collection.aggregate([
+            {
+                "$search" : {
+                    "autocomplete" : {
+                        "query": `${request.query.query}`,
+                        "path": "title",
+                        "fuzzy": {
+                            "maxEdits":2,
+                            "prefixlength":3
+                        }
+                    }
+                }
+            }
+        ]).toArray()
+        response.send(result)
+    }catch (error){
+        response.status(500).send({message: error.message})
+    }
+})
+
+app.get("/get/:id",async (request,response)=> {
+    try{
+        let result = await collection.findOne({
+            "_id" : ObjectId(request.params.id)
+        })
+        response.send(result)
+    }catch(error) {
+        response.status(500).send({message: error.message})
+    }
+})
+
+app.listen(process.env.PORT || PORT , () => {
+    console.log(`server is running on port ${PORT}`)
+})
