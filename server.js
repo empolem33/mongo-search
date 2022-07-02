6bf38d74f7d25bc90b9b61111e64e443891c5ebb
empolem33 <inspiron0925@yahoo.com>
2022-07-01 19:27:20 -0700

    search done

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const {MongoClient, ObjectID} = require("mongodb")
+const {MongoClient, ObjectId } = require('mongodb')
 const { response } = require('express')
+const { request } = require('http')
 require('dotenv').config()
 const PORT = 8000
 
@@ -13,7 +14,7 @@ let db,
 
 MongoClient.connect(dbConnectionStr)
     .then(client => {
-        console.log ('Connected to database boss mode on!')
+        console.log(`Connected to database`)
         db = client.db(dbName)
         collection = db.collection('movies')
     })
@@ -22,39 +23,44 @@ app.use(express.urlencoded({extended : true}))
 app.use(express.json())
 app.use(cors())
 
-app.get('/search', async (request, response) => {
-    try{
+app.get("/search", async (request,response) => {
+    try {
         let result = await collection.aggregate([
             {
-                "$Search" : {
+                "$search" : {
                     "autocomplete" : {
                         "query": `${request.query.query}`,
                         "path": "title",
                         "fuzzy": {
                             "maxEdits":2,
-                            "prefixlength":3
+                            "prefixLength": 3
                         }
                     }
                 }
             }
         ]).toArray()
+        // console.log(result)
         response.send(result)
-    }catch (error){
+    } catch (error) {
         response.status(500).send({message: error.message})
+        console.log(error)
     }
 })
 
-app.get("/get/:id",async (request,response)=> {
-    try{
+app.get("/get/:id", async (request, response) => {
+    try {
         let result = await collection.findOne({
             "_id" : ObjectId(request.params.id)
         })
         response.send(result)
-    }catch(error) {
+    } catch (error) {
         response.status(500).send({message: error.message})
     }
+}
+)
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server is running.`)
 })
 
-app.listen(process.env.PORT || PORT , () => {
-    console.log(`server is running on port ${PORT}`)
-})
+// THIS IS THE INDEX TO APPLY TO MONGODB MOVIES COLLECTION
